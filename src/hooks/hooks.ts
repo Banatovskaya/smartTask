ece90923fe07c04b4a5047f11e787980466069d5
Ira <banatovskaya@gmail.com>
2024-08-31 16:57:35 +0300

    add bindActionsCreators

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,6 +1,12 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import type { RootState, AppDispatch } from '../store/store';
+import { bindActionCreators } from "@reduxjs/toolkit";
+import { useMemo } from "react";
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
-export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppDispatch = (actionCreators:any) => {
+    const dispatch = useDispatch<AppDispatch>()
+    return useMemo(() =>
+        bindActionCreators(actionCreators, dispatch), [dispatch])
+};
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
